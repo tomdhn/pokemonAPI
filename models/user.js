696d10b53d312cfbe60b3a96f66492d4fb0eac34
dhoin <tom.dhoine@student.vives.be>
2024-05-19 12:02:06 +0200

    added user model

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const UserSchema = new mongoose.Schema({
+    admin: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    username: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 100,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 100,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 100
+    }
+});
+
+const UserModel = mongoose.model('user', UserSchema);
+
+const ValidateUser = (user) => {
+    const schema = Joi.object({
+        admin: Joi.boolean(),
+        username: Joi.string().min(1).max(100).required(),
+        email: Joi.string().min(1).max(100).required().email(),
+        password: Joi.string().min(1).max(100).required()
+    });
+
+    return schema.validate(user);
+}
+
+module.exports = { UserModel, ValidateUser };
